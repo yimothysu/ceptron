8fec429971b3af337ea1035079f73b9ea23ac06a
Jim Su <jimsu@protonmail.com>
2022-10-22 08:49:13 -0400

    Update font and command signature

--- frontend/commands.js
+++ frontend/commands.js
@@ -1,9 +1,16 @@
 const { generateImage, generateSummary } = require("./calls.js");
 
+function splitFirstSpace(str) {
+  const index = str.indexOf(" ");
+  return [str.substring(0, index), str.substring(index + 1)];
+}
+
 function processCommands(command) {
-  if (command.startsWith("/generate_image(")) {
+  let [cmd, args] = splitFirstSpace(command);
+
+  if (["image", "img", "i"].includes(cmd)) {
     generateImage(
-      command.substring(command.indexOf("(") + 1, command.length - 1)
+      args
     );
   }
 }
