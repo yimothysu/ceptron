13920c0f027896be303cd483f5db1a92e10d7c9c
Andes0113 <54319516+Andes0113@users.noreply.github.com>
2022-10-22 11:29:58 -0400

    Fetch Prelim + Descriptions

--- frontend/calls.js
+++ frontend/calls.js
@@ -2,7 +2,8 @@ async function generateImage(prompt) {
   return prompt;
   /*
 	Route: GET /generate/image
-	headers: include prompt
+	Headers: prompt
+	Return: image generated from stable diffusion
   */
   //   return fetch("https://localhost:8000/generate/image", {
   //     method: "GET",
@@ -18,6 +19,11 @@ async function generateImage(prompt) {
 
 async function generateSummary(url) {
   return url;
+  /*
+	Route: GET /generate/summary
+	Headers: prompt
+	Return: summary text generated from url with sumy
+  */
   //   return fetch("https://localhost:8000/generate/summary", {
   //     method: "GET",
   //     headers: {
