207998cc57f8fdb5f2a469ed2d6c0676fd719b79
Andes0113 <54319516+Andes0113@users.noreply.github.com>
2022-10-22 11:26:38 -0400

    Prelim Fetch

--- frontend/calls.js
+++ frontend/calls.js
@@ -4,31 +4,31 @@ async function generateImage(prompt) {
 	Route: GET /generate/image
 	headers: include prompt
   */
-  return fetch("https://localhost:8000/generate/image", {
-    method: "GET",
-    headers: {
-      prompt: prompt,
-    },
-  })
-    .then((res) => res.json)
-    .then((json) => {
-      return json;
-    });
+  //   return fetch("https://localhost:8000/generate/image", {
+  //     method: "GET",
+  //     headers: {
+  //       prompt: prompt,
+  //     },
+  //   })
+  //     .then((res) => res.json)
+  //     .then((json) => {
+  //       return json;
+  //     });
 }
 
 async function generateSummary(url) {
   return url;
-  return fetch("https://localhost:8000/generate/summary", {
-    method: "GET",
-    headers: {
-      prompt: prompt,
-    },
-  })
-    .then((res) => res.json)
-    .then((json) => {
-      return json;
-    });
-  return url;
+  //   return fetch("https://localhost:8000/generate/summary", {
+  //     method: "GET",
+  //     headers: {
+  //       prompt: prompt,
+  //     },
+  //   })
+  //     .then((res) => res.json)
+  //     .then((json) => {
+  //       return json;
+  //     });
+  //   return url;
 }
 module.exports = {
   generateImage,
