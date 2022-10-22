c63d95d9c8f12fae17bbc079e423669bbf016a7e
Andes0113 <54319516+Andes0113@users.noreply.github.com>
2022-10-21 22:53:41 -0400

    Global Shortcut

--- frontend/main.js
+++ frontend/main.js
@@ -1,23 +1,23 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path');
-const { electron } = require('process');
+const { app, BrowserWindow, globalShortcut } = require("electron");
+const path = require("path");
+const { electron } = require("process");
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
-//   const screenDimensions = electron.screen.getPrimaryDisplay().size.width - 200;
-//   const windowWidth = Math.round(screenDimensions.width * .50);
-//   const windowHeight = 100;
+  //   const screenDimensions = electron.screen.getPrimaryDisplay().size.width - 200;
+  //   const windowWidth = Math.round(screenDimensions.width * .50);
+  //   const windowHeight = 100;
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 100,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, "preload.js"),
     },
-	frame: false,
-  })
+    frame: false,
+  });
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile("index.html");
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
@@ -27,25 +27,23 @@ function createWindow () {
 // initialization and is ready to create browser windows.
 
 app.whenReady().then(() => {
-  createWindow()
-  const ret = globalShortcut.register('Command+C', () => {
-    console.log('Electron loves global shortcuts!')
-  })
+  const ret = globalShortcut.register("CommandOrControl+Shift+C", () => {
+    createWindow();
+  });
 
-
-  app.on('activate', function () {
+  app.on("activate", function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
