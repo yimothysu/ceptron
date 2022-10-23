ae4eccca56b0524383ad901f7619e53b2fd5f602
Andy Yu <ayu303@gatech.edu>
2022-10-23 00:50:30 -0400

    Bugfixes to API call

--- image.py
+++ image.py
@@ -1,19 +1,20 @@
 from fastapi import APIRouter
+from fastapi.responses import FileResponse
 import os
 import shlex
+import subprocess
+import time
 
 router = APIRouter()
 
 @router.get("/api/image")
 async def image(prompt: str):
     sanitizedPrompt = shlex.quote(prompt)
-    ckptPath = '/home/hydra/hackGT9/weights/'
-    scriptPath = '/home/hydra/hackGT9/stableDiffusionLib/scripts/text2img.py'
+    ckptPath = '/home/hydra/hackGT9/weights/sd-v1-4.ckpt'
+    scriptPath = '/home/hydra/hackGT9/stableDiffusionLib/scripts/txt2img.py'
     outdir = '/home/hydra/hackGT9/outputs/txt2img-samples/'
-    os.system(f'python {scriptPath} --prompt {sanitizedPrompt} --ckpt {ckptPath} --outdir {outdir}')
+    configPath = '/home/hydra/hackGT9/stableDiffusionLib/configs/stable-diffusion/v1-inference.yaml'
+    os.system(f'source /home/hydra/anaconda3/bin/activate;conda activate ldm;python3 {scriptPath} --prompt {sanitizedPrompt} --ckpt {ckptPath} --outdir {outdir} --config {configPath}')
+    time.sleep(15)
     outImagePath = outdir + 'samples/00000.png'
-    with open(outImagePath, "rb") as image:
-        outImage = image.read()
-        imageBytes = bytearray(outImage)
-    os.system(f'rm -r {outdir}*')
-    return imageBytes
+    return FileResponse(outImagePath)
