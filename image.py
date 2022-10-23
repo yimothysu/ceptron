492430e46e5b8c1e680d426fe1e8774a77570dcd
Andy Yu <ayu303@gatech.edu>
2022-10-23 01:54:17 -0400

    Speed Improvments
    Removed a few slowdowns in the code

--- image.py
+++ image.py
@@ -9,12 +9,16 @@ router = APIRouter()
 
 @router.get("/api/image")
 async def image(prompt: str):
+    os.system('sudo rm -r /home/hydra/hackGT9/outputs/txt2img-samples/*')
+    #time.sleep(0.1)
     sanitizedPrompt = shlex.quote(prompt)
     ckptPath = '/home/hydra/hackGT9/weights/sd-v1-4.ckpt'
     scriptPath = '/home/hydra/hackGT9/stableDiffusionLib/scripts/txt2img.py'
     outdir = '/home/hydra/hackGT9/outputs/txt2img-samples/'
     configPath = '/home/hydra/hackGT9/stableDiffusionLib/configs/stable-diffusion/v1-inference.yaml'
-    os.system(f'source /home/hydra/anaconda3/bin/activate;conda activate ldm;python3 {scriptPath} --prompt {sanitizedPrompt} --ckpt {ckptPath} --outdir {outdir} --config {configPath}')
-    time.sleep(15)
+    #os.system(f'source /home/hydra/anaconda3/bin/activate;conda activate ldm;python3 {scriptPath} --prompt {sanitizedPrompt} --ckpt {ckptPath} --outdir {outdir} --config {configPath}')
+    os.system(f'python3 {scriptPath} --prompt {sanitizedPrompt} --ckpt {ckptPath} --outdir {outdir} --config {configPath}')
+
+    #time.sleep(1)
     outImagePath = outdir + 'samples/00000.png'
     return FileResponse(outImagePath)
